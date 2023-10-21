74bbc59b5a8e6069d8973029f66024903a2d96ef
Derenik <deren.kha@gmail.com>
2023-10-22 01:39:38 +0400

    init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
-
+const app = new Elysia()
+  .ws("/ws", {
+    message(ws, message) {
+      console.log(message);
+      ws.send({ message, date: new Date() });
+    },
+  })
+  .listen(3000);
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
